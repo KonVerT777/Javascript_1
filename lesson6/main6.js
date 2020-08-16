2f0947f4e8f6f6a67d135c53cda7bd44bc5c26b2
Vera <kondrashinavera@gmail.com>
2020-08-17 00:00:41 +0300

    Lesson_6

---
+++ lesson6/main6.js
@@ -0,0 +1,37 @@
+var basket = {};
+
+var products = {
+    "ear" : {
+        name: 'Сережки', price: 40, image: "img/earrings.jpg"
+    },
+    "bra" : {
+        name: 'Браслет', price: 150, image: "img/bracelet.jpg"
+    },
+    "ring" : {
+        name: 'Кольцо', price: 100, image: "img/ring.jpg"}
+    }
+
+var catalog = "";
+for(var key in products) {
+    catalog+="Изделие: " + products[key].name + "<br>";
+    catalog+="Стоимость, евро: " + products[key].price + "<br>";
+    catalog+='<img src="' + products[key].image + '">' + "<br>";
+    catalog+='<button class="btn" data-art="'+ key +'">Купить</button>';
+    catalog+='<hr>';
+}
+
+document.getElementById('catalog').innerHTML = catalog;
+document.querySelector('button').addEventListener('click', addToBasket);
+
+function addToBasket() {
+    var articul = this.getAttribute('data-art');
+    if(basket[articul] !== undefined) {
+        basket[articul]++;
+    } else {
+        basket[articul] = 1;
+    }
+    
+    console.log(basket);
+}
+
+
